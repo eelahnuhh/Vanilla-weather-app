0446133f72f7f17997f80c61872baf02f10881f6
eelahnuhh <ilanasmalls2002@gmail.com>
2022-12-24 16:03:20 -0600

    Changed the search and current location button styling

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ function getTemperature(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   getForecast(response.data.coord);
 }
